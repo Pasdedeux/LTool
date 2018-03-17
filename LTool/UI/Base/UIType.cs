0f0a19bfdc2f0733a86094ad0ffc98308c743d93
DerekLiu <lc10278@163.com>
2018-03-17 14:49:40 +0800

    修改下UI组建中的默认参数

--- LTool/UI/Base/UIType.cs
+++ LTool/UI/Base/UIType.cs
@@ -11,16 +11,16 @@ public class UIType
     /// <summary>
     /// UI窗体挂载的节点类型
     /// </summary>
-    public UITypeEnum uiType = UITypeEnum.PopUp;
+    public UITypeEnum uiType = UITypeEnum.Normal;
     /// <summary>
     /// UI窗体显示方式
     /// Normal - 示窗体与其他窗体可以同时显示
     /// PopUp - 多层弹窗，同时需要维护多个弹出窗口并依次恢复显示的情况
     /// Unique - 独占窗口，显示时其他界面隐藏
     /// </summary>
-    public UIShowModeEnum uiShowMode = UIShowModeEnum.PopUp;
+    public UIShowModeEnum uiShowMode = UIShowModeEnum.Normal;
     /// <summary>
     /// UI窗体透明度类型
     /// </summary>
-    public UITransparentEnum uiTransparent = UITransparentEnum.NoPenetratingMiddle;
+    public UITransparentEnum uiTransparent = UITransparentEnum.Penetrating;
 }

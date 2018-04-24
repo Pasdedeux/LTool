e9be01cc3d0bb0c615612a77ac9f0f3f08de3db8
Pasdedeux <lc10278@163.com>
2018-04-24 18:26:44 +0800

    UI更新

--- LTool/UI/Base/BaseUI.cs
+++ LTool/UI/Base/BaseUI.cs
@@ -1,19 +1,15 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class BaseUI : MonoBehaviour
+
+public abstract class BaseUI : MonoBehaviour
 {
-
-    private UIType _currentUIType = new UIType();
     /// <summary>
     /// 当前窗口类型
     /// </summary>
+    /// 
+    private UIType _uiType = new UIType();
     public UIType CurrentUIType
-    {
-        get { return _currentUIType; }
-        set { _currentUIType = value; }
-    }
+    { get { return _uiType; } set { _uiType = value; } }
 
     /// <summary>
     /// 显示窗体
@@ -23,27 +19,33 @@ public class BaseUI : MonoBehaviour
     {
         gameObject.SetActive( true );
         //设置模态窗体调用(弹出窗体)
-        if ( _currentUIType.uiType == UITypeEnum.PopUp )
-            UIMaskManager.Instance.SetMaskWindow( gameObject , _currentUIType.uiTransparent );
+        if( CurrentUIType.uiType == UITypeEnum.PopUp )
+            UIMaskManager.Instance.SetMaskWindow( gameObject , CurrentUIType.uiTransparent );
     }
 
     /// <summary>
     /// 隐藏窗口
     /// </summary>
-    /// <param name="freeze">是否暂时冻结（仍在栈中）</param>
+    /// <param name="freeze">是否暂时冻结（功能未想好）</param>
     public virtual void Hide( bool freeze = false )
     {
-        if ( !freeze )
+        if( !freeze )
         {
             gameObject.SetActive( false );
-            if ( _currentUIType.uiType == UITypeEnum.PopUp )
+            if( CurrentUIType.uiType == UITypeEnum.PopUp )
                 UIMaskManager.Instance.CancelMaskWindow();
         }
         else
         {
-            //对于处于冻结的UI，可能需要断开该窗口的网络通信或者操作、刷新响应等操作
-            gameObject.SetActive( true );
+            //TODO 对于处于冻结的UI，可能需要断开该窗口的网络通信或者操作、刷新响应等操作
+            gameObject.SetActive( false );
         }
+    }
+
+
+    public virtual void DoDestroy()
+    {
+        GameObject.Destroy( gameObject );
     }
 
 }

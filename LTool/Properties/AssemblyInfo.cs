a1ce6f07e1e44deba7acd4a4b7ce9fe3a693beae
DerekLiu <lc10278@163.com>
2018-05-06 14:13:30 +0800

    修改库信息

--- LTool/Properties/AssemblyInfo.cs
+++ LTool/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
 [assembly: AssemblyTitle("LitFramework")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription( "Derek Liu 自建Unity工具集及框架" )]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("LitFramework")]

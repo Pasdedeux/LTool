b546c1c6a0da0f7f0245174e3fb5c6cb42208626
DerekLiu <lc10278@163.com>
2018-05-06 13:08:28 +0800

    非预制件绑定版本UI框架

---
+++ LTool/UI/HotFix/BaseUI.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace LTool.HotFix
+{
+
+    public abstract class BaseUI
+    {
+        /// <summary>
+        /// 该窗口是否开启中
+        /// </summary>
+        public bool IsShowing { get; set; }
+        /// <summary>
+        /// 当前窗口类型
+        /// </summary>
+        private UIType _uiType = new UIType();
+        public UIType CurrentUIType
+        { get { return _uiType; } set { _uiType = value; } }
+
+        /// <summary>
+        /// 关联的UI实例
+        /// </summary>
+        public GameObject GameObjectInstance { get; set; }
+
+        /// <summary>
+        /// 资源名
+        /// </summary>
+        public string AssetsName { get; set; }
+
+        /// <summary>
+        /// 显示窗体
+        /// </summary>
+        /// <param name="replay">【暂时不管这个参数】</param>
+        public void Show( bool replay = false )
+        {
+            IsShowing = true;
+
+            //默认执行OnEnable()
+            GameObjectInstance.SetActive( IsShowing );
+
+            //设置模态窗体调用(弹出窗体)
+            if( CurrentUIType.uiNodeType == UINodeTypeEnum.PopUp )
+                UIMaskManager.Instance.SetMaskWindow( GameObjectInstance , CurrentUIType.uiTransparent );
+
+            OnEnabled();
+            OnShow();
+        }
+
+        /// <summary>
+        /// 隐藏窗口
+        /// </summary>
+        /// <param name="isDestroy">是否摧毁并彻底释放</param>
+        /// <param name="freeze">是否暂时冻结（功能未想好）</param>
+        public void Close( bool isDestroy = false , bool freeze = false )
+        {
+            OnDisabled();
+
+            //默认执行OnDisable()
+            if( !freeze )
+            {
+                GameObjectInstance.SetActive( false );
+
+                if( CurrentUIType.uiNodeType == UINodeTypeEnum.PopUp && IsShowing )
+                    UIMaskManager.Instance.CancelMaskWindow();
+            }
+            else
+            {
+                //TODO 对于处于冻结的UI，可能需要断开该窗口的网络通信或者操作、刷新响应等操作
+                GameObjectInstance.SetActive( false );
+            }
+
+            IsShowing = false;
+
+            if( isDestroy )
+                DoDestroy();
+        }
+
+
+        ///<inheritdoc/>
+        /// <remarks>
+        /// 刷新窗体
+        /// </remarks>
+        public abstract void OnShow();
+
+        public virtual void Dispose() { }
+
+        public virtual void OnAdapter() { }
+
+        #region Alternative Function
+
+        public abstract void OnAwake();
+
+        public abstract void OnEnabled();
+
+        public abstract void OnDisabled();
+
+        public virtual void OnStart() { }
+
+        public virtual void OnUpdate() { }
+
+        private void DoDestroy()
+        {
+            Dispose();
+            GameObject.Destroy( GameObjectInstance );
+            Resources.UnloadUnusedAssets();
+        }
+        #endregion
+
+    }
+}
---
+++ LTool/UI/HotFix/UIManager.cs
@@ -0,0 +1,543 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using LTool;
+using System.Reflection;
+using System.Linq;
+
+namespace LTool.HotFix
+{
+    /// <summary>
+    /// 以下UI类，需配合UImanager.unitypackage使用。
+    /// 
+    /// 主要包含Cavas_Root及相关Tag等
+    /// 
+    /// </summary>
+    public class UIManager : Singleton<UIManager>
+    {
+        /// <summary>
+        /// 所有的预制件名称列表
+        /// </summary>
+        private Dictionary<string , string> _allRegisterUIDict;
+        /// <summary>
+        /// //定义“栈”集合,存储显示当前所有弹出窗口的窗体类型
+        /// </summary>
+        private Stack<BaseUI> _stackCurrentUI;
+        /// <summary>
+        /// 缓存已经开启过的所有窗体
+        /// </summary>
+        private Dictionary<string , BaseUI> _dictLoadedAllUIs;
+        /// <summary>
+        /// 当前显示的非弹出类UI窗体
+        /// </summary>
+        private Dictionary<string , BaseUI> _dictCurrentShowUIs;
+        /// <summary>
+        /// UI根节点
+        /// </summary>
+        private Transform _transCanvas = null;
+        /// <summary>
+        /// 普通窗口节点
+        /// </summary>
+        private Transform _transNormal = null;
+        /// <summary>
+        /// 固定UI节点
+        /// </summary>
+        private Transform _transFixed = null;
+        /// <summary>
+        /// 弹出窗口节点
+        /// </summary>
+        private Transform _transPopUp = null;
+        /// <summary>
+        /// 管理器节点
+        /// </summary>
+        private Transform _transManager = null;
+        /// <summary>
+        /// 外部传入UI的加载方法。Resource.Load || AssetBundle.Load
+        /// </summary>
+        public Func<string , GameObject> GetUIResource;
+
+
+        public void Install( Func<string , GameObject> loadPrefabFunction )
+        {
+            _allRegisterUIDict = new Dictionary<string , string>();
+            _stackCurrentUI = new Stack<BaseUI>();
+            _dictLoadedAllUIs = new Dictionary<string , BaseUI>();
+            _dictCurrentShowUIs = new Dictionary<string , BaseUI>();
+
+            _transCanvas = GameObject.FindGameObjectWithTag( UISysDefine.SYS_TAG_ROOTCANVAS ).transform;
+            _transNormal = UnityHelper.FindTheChildNode( _transCanvas.gameObject , UISysDefine.SYS_TAG_NORMALCANVAS );
+            _transFixed = UnityHelper.FindTheChildNode( _transCanvas.gameObject , UISysDefine.SYS_TAG_FIXEDCANVAS );
+            _transPopUp = UnityHelper.FindTheChildNode( _transCanvas.gameObject , UISysDefine.SYS_TAG_POPUPCANVAS );
+            _transManager = UnityHelper.FindTheChildNode( _transCanvas.gameObject , UISysDefine.SYS_TAG_MANAGERCANVAS );
+
+            GetUIResource = loadPrefabFunction;
+        }
+
+        public void Uninstall()
+        {
+            while( _stackCurrentUI.Count > 0 )
+            {
+                BaseUI ui = _stackCurrentUI.Pop();
+                ui.Close( true );
+            }
+
+            if( _dictLoadedAllUIs != null )
+            {
+                var list = _dictLoadedAllUIs.ToList();
+                for( int i = list.Count - 1; i >= 0; i-- )
+                    Close( list[ i ].Key , true );
+                list = null;
+            }
+            _allRegisterUIDict.Clear();
+            _stackCurrentUI.Clear();
+            _dictCurrentShowUIs.Clear();
+
+            _transFixed = null;
+            _transPopUp = null;
+            _transCanvas = null;
+            _transNormal = null;
+            _transManager = null;
+            _stackCurrentUI = null;
+            _allRegisterUIDict = null;
+            _dictLoadedAllUIs = null;
+            _dictCurrentShowUIs = null;
+
+            GetUIResource = null;
+
+            Resources.UnloadUnusedAssets();
+        }
+
+
+
+        /// <summary>
+        /// 显示（打开）UI窗口
+        /// 功能：
+        /// 1、根据UI窗体的名称，加载到UI窗口缓存列表
+        /// 2、根据不同UI显示模式，做不同的加载处理
+        /// </summary>
+        /// <param name="uiName">UI窗体预制件名称</param>
+        public BaseUI Show( string uiName )
+        {
+            BaseUI baseUI = null;
+
+            if( string.IsNullOrEmpty( uiName ) )
+                throw new Exception( "UI--uiName 为 Null" );
+
+            //根据名称加载窗体到UI窗体缓存中
+            baseUI = LoadUIToAndFromAllList( uiName );
+            if( baseUI == null )
+                throw new Exception( "UI--baseUI 加载失败" );
+
+            //判断是否清空“栈”结构体集合
+            if( baseUI.CurrentUIType.isClearPopUp )
+                ClearPopUpStackArray();
+
+            switch( baseUI.CurrentUIType.uiShowMode )
+            {
+                case UIShowModeEnum.Normal:
+                    LoadNormalUI( uiName );
+                    break;
+                case UIShowModeEnum.PopUp:
+                    LoadPopUpUI( uiName );
+                    break;
+                case UIShowModeEnum.Unique:
+                    LoadUniqueUI( uiName );
+                    break;
+                default:
+                    throw new Exception( "未登记的UI类型--" + baseUI.CurrentUIType.uiShowMode );
+            }
+
+            return baseUI;
+        }
+
+        /// <summary>
+        /// 关闭指定UI
+        /// </summary>
+        /// <param name="uiName"></param>
+        /// <param name="isDestroy">是否直接释放所有资源，销毁</param>
+        public void Close( string uiName , bool isDestroy = false )
+        {
+            if( string.IsNullOrEmpty( uiName ) )
+                return;
+
+            BaseUI baseUI = null;
+
+            //所有窗体如果没有记录，直接返回
+            _dictLoadedAllUIs.TryGetValue( uiName , out baseUI );
+            if( baseUI == null )
+            {
+                _dictLoadedAllUIs.Remove( uiName );
+                return;
+            }
+
+            //不同类型窗体执行各自关闭逻辑
+            switch( baseUI.CurrentUIType.uiShowMode )
+            {
+                case UIShowModeEnum.Normal:
+                    UnLoadNormalUI( uiName , isDestroy );
+                    break;
+                case UIShowModeEnum.PopUp:
+                    UnLoadPopUpUI( uiName , isDestroy );
+                    break;
+                case UIShowModeEnum.Unique:
+                    UnLoadUniqueUI( uiName , isDestroy );
+                    break;
+                default:
+                    throw new Exception( "未登记的UI类型--" + baseUI.CurrentUIType.uiShowMode );
+            }
+
+            //销毁
+            if( isDestroy )
+            {
+                BaseUI ui = null;
+                _dictLoadedAllUIs.TryGetValue( uiName , out ui );
+                if( ui != null )
+                    ui.Close( true );
+                _dictLoadedAllUIs.Remove( uiName );
+            }
+        }
+
+
+
+        /// <summary>
+        /// 清空弹出窗口栈
+        /// </summary>
+        public bool ClearPopUpStackArray()
+        {
+            if( _stackCurrentUI != null && _stackCurrentUI.Count > 0 )
+            {
+                foreach( var stackUI in _stackCurrentUI )
+                {
+                    stackUI.Close();
+                }
+                _stackCurrentUI.Clear();
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// 根据UI预制件名称加载到UI缓存列表（按需），同时获取实例
+        /// </summary>
+        /// <param name="uiName"></param>
+        /// <returns></returns>
+        private BaseUI LoadUIToAndFromAllList( string uiName )
+        {
+            BaseUI result = null;
+            _dictLoadedAllUIs.TryGetValue( uiName , out result );
+            if( result == null )
+                result = LoadUI( uiName );
+            return result;
+        }
+
+        /// <summary>
+        /// 加载指定名称UI
+        /// 功能：
+        /// 1、根据名称加载预制体
+        /// 2、根据UI类型加载到不同节点下
+        /// 3、隐藏创建的UI克隆体
+        /// 4、把克隆体加入到所有窗体列表
+        /// </summary>
+        /// <param name="uiName">窗体名称</param>
+        /// <returns></returns>
+        private BaseUI LoadUI( string uiName )
+        {
+            //加载的UI预制体
+            BaseUI baseUI = null;
+            GameObject prefClone = null;
+
+            //加载预制体
+            if( !string.IsNullOrEmpty( uiName ) )
+                prefClone = GetUIResource != null ? GetUIResource( uiName ) : null;
+            if( prefClone == null )
+                throw new Exception( "未指定UI预制件加载方法或UI预制件路径指定错误 ==>" + uiName );
+            prefClone = GameObject.Instantiate( prefClone );
+
+            //设置父节点
+            if( _transCanvas != null && prefClone != null )
+            {
+                if( _allRegisterUIDict.ContainsKey( uiName ) )
+                {
+                    baseUI = Activator.CreateInstance( Type.GetType( _allRegisterUIDict[ uiName ] , true , true ) ) as BaseUI;
+                    baseUI.GameObjectInstance = prefClone;
+                    baseUI.AssetsName = uiName;
+                }
+
+                baseUI.OnAwake();
+                baseUI.OnAdapter();
+
+                switch( baseUI.CurrentUIType.uiNodeType )
+                {
+                    case UINodeTypeEnum.Normal:
+                        prefClone.transform.SetParent( _transNormal , false );
+                        break;
+                    case UINodeTypeEnum.Fixed:
+                        prefClone.transform.SetParent( _transFixed , false );
+                        break;
+                    case UINodeTypeEnum.PopUp:
+                        prefClone.transform.SetParent( _transPopUp , false );
+                        break;
+                    default:
+                        throw new Exception( "未登记的UI类型--" + baseUI.CurrentUIType.uiShowMode );
+                }
+
+                //加入到所有窗体缓存中
+                _dictLoadedAllUIs.Add( uiName , baseUI );
+                return baseUI;
+            }
+
+            Debug.Log( "窗体加载失败！" );
+            return null;
+        }
+
+        /// <summary>
+        /// 加载当前窗体到当前窗体集合
+        /// </summary>
+        /// <param name="uiName"></param>
+        private void LoadNormalUI( string uiName )
+        {
+            BaseUI baseUI;
+            _dictCurrentShowUIs.TryGetValue( uiName , out baseUI );
+            if( baseUI != null )
+            {
+                if( baseUI.IsShowing )
+                    baseUI.OnShow();
+                else
+                    baseUI.Show();
+                return;
+            }
+
+            //加载当前窗体到当前显示集合
+            _dictLoadedAllUIs.TryGetValue( uiName , out baseUI );
+            if( baseUI != null )
+            {
+                _dictCurrentShowUIs.Add( uiName , baseUI );
+                baseUI.Show();
+            }
+        }
+
+        /// <summary>
+        /// 从当前UI列表缓存中卸载UI窗体
+        /// </summary>
+        /// <param name="uiName"></param>
+        private void UnLoadNormalUI( string uiName , bool isDestroy = false )
+        {
+            BaseUI baseUI;
+
+            //当前UI显示列表中没有记录或者总表中没有记录则直接返回
+            _dictCurrentShowUIs.TryGetValue( uiName , out baseUI );
+            if( baseUI == null )
+            {
+                if( !isDestroy )
+                    return;
+                else
+                    _dictLoadedAllUIs.TryGetValue( uiName , out baseUI );
+
+                if( baseUI == null )
+                    return;
+            }
+            else
+                //隐藏窗口并从列表中移除
+                _dictCurrentShowUIs.Remove( uiName );
+
+            baseUI.Close( isDestroy: isDestroy );
+        }
+
+        /// <summary>
+        /// 加载独占UI窗体
+        /// </summary>
+        /// <param name="uiName"></param>
+        private void LoadUniqueUI( string uiName )
+        {
+            BaseUI baseUI;
+            //当前UI显示列表中没有记录则直接返回
+            _dictCurrentShowUIs.TryGetValue( uiName , out baseUI );
+            if( baseUI != null )
+            {
+                if( baseUI.IsShowing )
+                    baseUI.OnShow();
+                else
+                    baseUI.Show();
+                return;
+            }
+
+            //正在显示的UI进行隐藏
+            foreach( BaseUI baseui in _dictCurrentShowUIs.Values )
+            {
+                baseui.Close();
+            }
+            foreach( BaseUI baseui in _stackCurrentUI )
+            {
+                baseui.Close();
+            }
+
+            //把当前窗体加载到正显示的UI窗口缓存中去
+            _dictLoadedAllUIs.TryGetValue( uiName , out baseUI );
+            if( baseUI != null )
+            {
+                _dictCurrentShowUIs.Add( uiName , baseUI );
+                baseUI.Show();
+            }
+
+        }
+
+        /// <summary>
+        /// 卸载当前UI，并将原先被隐藏的UI重新显示
+        /// </summary>
+        /// <param name="uiName"></param>
+        private void UnLoadUniqueUI( string uiName , bool isDestroy = false )
+        {
+            BaseUI baseUI;
+
+            _dictCurrentShowUIs.TryGetValue( uiName , out baseUI );
+            if( baseUI == null )
+            {
+                if( !isDestroy )
+                    return;
+                else
+                    _dictLoadedAllUIs.TryGetValue( uiName , out baseUI );
+
+                if( baseUI == null )
+                    return;
+            }
+            else
+                //隐藏窗口并从列表中移除
+                _dictCurrentShowUIs.Remove( uiName );
+
+            baseUI.Close( isDestroy: isDestroy );
+
+            //如果需要清空已有 popup 窗口
+            if( baseUI.CurrentUIType.isClearPopUp )
+                ClearPopUpStackArray();
+
+            //正在显示的窗口和栈缓存的窗口再次进行显示处理
+            foreach( BaseUI baseui in _dictCurrentShowUIs.Values )
+            {
+                baseui.Show( true );
+            }
+            foreach( BaseUI baseui in _stackCurrentUI )
+            {
+                baseui.Show( true );
+            }
+        }
+
+        /// <summary>
+        /// 弹出窗口，入栈
+        /// 先冻结栈中窗口，再将此窗口入栈
+        /// </summary>
+        /// <param name="uiName"></param>
+        private void LoadPopUpUI( string uiName )
+        {
+            BaseUI baseUI;
+
+            //判断栈里是否有窗口，有则冻结响应
+            if( _stackCurrentUI.Count > 0 )
+            {
+                BaseUI topUI = _stackCurrentUI.Peek();
+                topUI.Close( freeze: true );
+            }
+
+            //获取当前UI，进行展示处理
+            _dictLoadedAllUIs.TryGetValue( uiName , out baseUI );
+            if( baseUI != null )
+            {
+                if( baseUI.IsShowing )
+                    baseUI.OnShow();
+                else
+                    baseUI.Show();
+
+                if( !_stackCurrentUI.Contains( baseUI ) )
+                    //该弹出UI入栈
+                    _stackCurrentUI.Push( baseUI );
+            }
+            else
+                throw new Exception( "UIManager catch an error" );
+        }
+
+        /// <summary>
+        /// 弹出窗口，出栈
+        /// </summary>
+        /// <param name="uiName"></param>
+        private void UnLoadPopUpUI( string uiName , bool isDestroy = false )
+        {
+            //有两个以上弹窗出现时
+            if( _stackCurrentUI.Count >= 2 )
+            {
+                //第一个出栈
+                BaseUI topUI = _stackCurrentUI.Pop();
+                topUI.Close( isDestroy: isDestroy );
+
+                //第二个重新显示
+                BaseUI nextUI = _stackCurrentUI.Peek();
+                nextUI.Show( true );
+            }
+            //当前只有一个弹窗
+            else if( _stackCurrentUI.Count == 1 )
+            {
+                //出栈的窗体进行隐藏
+                BaseUI topUI = _stackCurrentUI.Pop();
+                topUI.Close( isDestroy: isDestroy );
+            }
+        }
+
+        /// <summary>
+        /// 从现有缓存中查找目标UI，未加载则返回null
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public BaseUI GetUIByName( string name )
+        {
+            BaseUI baseUI;
+            _dictLoadedAllUIs.TryGetValue( name , out baseUI );
+            return baseUI;
+        }
+
+        #region 反射方法，用于热更时UI绑定
+
+        /// <summary>
+        /// 反射注册UI回调
+        /// </summary>
+        /// <param name="_assetsName"></param>
+        /// <param name="_className"></param>
+        public void RegistFunctionCallFun( string uiPathName , string className )
+        {
+            if( !String.IsNullOrEmpty( uiPathName ) && !_allRegisterUIDict.ContainsKey( uiPathName ) )
+                _allRegisterUIDict.Add( uiPathName , className );
+
+            Debug.LogError( "LTOOL UI添加成功 " + uiPathName );
+        }
+
+        private void Reflection()
+        {
+            System.Reflection.Assembly asb = System.Reflection.Assembly.GetExecutingAssembly();
+
+            System.Type[] assemblyTypes = asb.GetTypes();
+
+            for( int indexType = 0; indexType < assemblyTypes.Length; indexType++ )
+            {
+                if( !assemblyTypes[ indexType ].IsAbstract && assemblyTypes[ indexType ].BaseType == typeof( BaseUI ) )
+                {
+                    //通过程序集获取到他的返回实例对象方法  并且初始化对象
+                    System.Reflection.MethodInfo mif = assemblyTypes[ indexType ].GetMethod( "RegistSystem" );
+                    if( mif != null )
+                    {
+                        //目前只认静态方法
+                        if( mif.IsStatic )
+                            mif.Invoke( null , new object[] { assemblyTypes[ indexType ].Namespace + "." + assemblyTypes[ indexType ].Name } );
+                        //else
+                        //{
+                        //    //TODO 需要实例化的带参数方法
+                        //    ConstructorInfo magicConstructor = assemblyTypes[ indexType ].GetConstructor( Type.EmptyTypes );
+                        //    object magicClassObject = magicConstructor.Invoke( new object[] { } );
+                        //    mif.Invoke( magicClassObject , new object[] { assemblyTypes[ indexType ].Name } );
+                        //}
+                    }
+                }
+            }
+        }
+
+        #endregion
+
+    }
+}

6aefc86165cc23bfafddefeff7a8c26e0b275756
Pasdedeux <lc10278@163.com>
2018-09-09 16:19:44 +0800

    更新库中第三方插件 高亮

---
+++ LTool/Third_Plugins/HighlightingSystem/Editor/HighlightingRendererEditor.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+namespace HighlightingSystem
+{
+	[CustomEditor(typeof(HighlightingRenderer), true)]
+	public class HighlightingRendererEditor { }
+	
+}

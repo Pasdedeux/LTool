bce1f8f16ca1be4366f89ea418a95d39cb115074
pasdedeux <lc10278@163.com>
2019-02-20 10:45:27 +0800

    增加相机部分扩展方法

--- LTool/LitTool/CSExtention.cs
+++ LTool/LitTool/CSExtention.cs
@@ -63,5 +63,80 @@ namespace LitFramework.LitTool
             func = null;
         }
         #endregion
+
+        #region 相机相关
+        /// <summary>
+        /// 获取【透视相机】指定距离下相机视口四个角的坐标
+        /// </summary>
+        /// <param name="cam"></param>
+        /// <param name="distance">相对于相机的距离</param>
+        /// <returns></returns>
+        public static void GetCameraBounds( this Camera cam, float distance , ref Vector3[] corners )
+        {
+            //Vector3[] corners = new Vector3[ 4 ];
+
+            float halfFOV = ( cam.fieldOfView * 0.5f ) * Mathf.Deg2Rad;
+            float aspect = cam.aspect;
+
+            float height = distance * Mathf.Tan( halfFOV );
+            float width = height * aspect;
+
+            Transform tx = cam.transform;
+
+            // 左上角
+            corners[ 0 ] = tx.position - ( tx.right * width );
+            corners[ 0 ] += tx.up * height;
+            corners[ 0 ] += tx.forward * distance;
+
+            // 右上角
+            corners[ 1 ] = tx.position + ( tx.right * width );
+            corners[ 1 ] += tx.up * height;
+            corners[ 1 ] += tx.forward * distance;
+
+            // 左下角
+            corners[ 2 ] = tx.position - ( tx.right * width );
+            corners[ 2 ] -= tx.up * height;
+            corners[ 2 ] += tx.forward * distance;
+
+            // 右下角
+            corners[ 3 ] = tx.position + ( tx.right * width );
+            corners[ 3 ] -= tx.up * height;
+            corners[ 3 ] += tx.forward * distance;
+
+            //return corners;
+        }
+
+        /// <summary>
+        /// 获取【正交相机】视口四个角的坐标
+        /// </summary>
+        /// <param name="cam"></param>
+        /// <returns></returns>
+        public static void GetCameraBounds( this Camera cam , ref Vector3[] corners )
+        {
+            float aspect = cam.aspect;
+            float halfHeight = cam.orthographicSize;
+            float width = halfHeight * aspect;
+
+            Transform tx = cam.transform;
+
+            // 左上角
+            corners[ 0 ] = tx.position - ( tx.right * width );
+            corners[ 0 ] += tx.up * halfHeight;
+
+            // 右上角
+            corners[ 1 ] = tx.position + ( tx.right * width );
+            corners[ 1 ] += tx.up * halfHeight;
+
+            // 左下角
+            corners[ 2 ] = tx.position - ( tx.right * width );
+            corners[ 2 ] -= tx.up * halfHeight;
+
+            // 右下角
+            corners[ 3 ] = tx.position + ( tx.right * width );
+            corners[ 3 ] -= tx.up * halfHeight;
+
+            //return corners;
+        }
+        #endregion
     }
 }

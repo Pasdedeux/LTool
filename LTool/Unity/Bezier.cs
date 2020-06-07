ae95144d21e6a637acb5b3e838da27a48a3c34f3
Pasdedeux <lc10278@163.com>
2020-06-07 15:06:52 +0800

    添加二阶贝塞尔曲线控制公式

---
+++ LTool/Unity/Bezier.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Bezier : MonoBehaviour
+{
+    public Transform[] controlPoints;
+    public LineRenderer lineRenderer;
+
+    public int layerOrder = 0;
+    private int _segmentNum = 10;
+
+
+    void Start()
+    {
+        if ( !lineRenderer )
+        {
+            lineRenderer = GetComponent<LineRenderer>();
+        }
+        lineRenderer.sortingLayerID = layerOrder;
+
+        //DrawCurve();
+    }
+
+    void Update()
+    {
+
+        DrawCurve();
+
+    }
+
+    void DrawCurve()
+    {
+        for ( int i = 1; i <= _segmentNum; i++ )
+        {
+            float t = i / ( float )_segmentNum;
+            int nodeIndex = 0;
+            Vector3 pixel = BezierUtils.CalculateCubicBezierPoint( t, controlPoints[ nodeIndex ].position,
+                controlPoints[ nodeIndex + 1 ].position, controlPoints[ nodeIndex + 2 ].position );
+            lineRenderer.positionCount = i;
+            lineRenderer.SetPosition( i - 1, pixel );
+        }
+
+    }
+
+    public class BezierUtils
+    {
+        /// <summary>
+        /// 根据T值，计算贝塞尔曲线上面相对应的点
+        /// 
+        /// 二阶公式：result = (1-t)srq2p0 + 2t(1-t)p1 + tsqr2p2
+        /// </summary>
+        /// <param name="t"></param>T值
+        /// <param name="p0"></param>起始点
+        /// <param name="p1"></param>控制点
+        /// <param name="p2"></param>目标点
+        /// <returns></returns>根据T值计算出来的贝赛尔曲线点
+        public static Vector3 CalculateCubicBezierPoint( float t, Vector3 p0, Vector3 p1, Vector3 p2 )
+        {
+            float u = 1 - t;
+            float tt = t * t;
+            float uu = u * u;
+
+            Vector3 p = uu * p0;
+            p += 2 * u * t * p1;
+            p += tt * p2;
+
+            return p;
+        }
+
+        /// <summary>
+        /// 获取存储贝塞尔曲线点的数组
+        /// </summary>
+        /// <param name="startPoint"></param>起始点
+        /// <param name="controlPoint"></param>控制点
+        /// <param name="endPoint"></param>目标点
+        /// <param name="segmentNum"></param>采样点的数量
+        /// <returns></returns>存储贝塞尔曲线点的数组
+        public static Vector3[] GetBeizerList( Vector3 startPoint, Vector3 controlPoint, Vector3 endPoint, int segmentNum )
+        {
+            Vector3[] path = new Vector3[ segmentNum ];
+            for ( int i = 1; i <= segmentNum; i++ )
+            {
+                float t = i / ( float )segmentNum;
+                Vector3 pixel = CalculateCubicBezierPoint( t, startPoint,
+                    controlPoint, endPoint );
+                path[ i - 1 ] = pixel;
+                Debug.Log( path[ i - 1 ] );
+            }
+            return path;
+
+        }
+    }
+}

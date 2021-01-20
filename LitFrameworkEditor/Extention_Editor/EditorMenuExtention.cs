4bf22d7f7035ca60ce743ad1c9943aef5e4617b1
pasdedeux <lc10278@163.com>
2021-01-20 17:10:23 +0800

    扩展编辑器：增加查找引用了当前对象的预制件

--- LitFrameworkEditor/Extention_Editor/EditorMenuExtention.cs
+++ LitFrameworkEditor/Extention_Editor/EditorMenuExtention.cs
@@ -332,4 +332,67 @@ namespace LitFrameworkEditor.EditorExtended
         }
 #endif
     }
+
+
+    /// <summary>
+    /// 查找引用预制件窗口
+    /// </summary>
+    public class SearchRefrenceEditorWindow : EditorWindow
+    {
+        /// <summary>
+        /// 查找引用
+        /// </summary>
+        [MenuItem( "Tools/查找引用的预制件" )]
+        static void SearchRefrence()
+        {
+            SearchRefrenceEditorWindow window = ( SearchRefrenceEditorWindow )EditorWindow.GetWindow( typeof( SearchRefrenceEditorWindow ), false, "Searching", true );
+            window.Show();
+        }
+
+        private static Object _searchObject;
+        private List<Object> _result = new List<Object>();
+        private void OnGUI()
+        {
+            EditorGUILayout.BeginHorizontal();
+            _searchObject = EditorGUILayout.ObjectField( _searchObject, typeof( Object ), true, GUILayout.Width( 200 ) );
+            if ( GUILayout.Button( "查找", GUILayout.Width( 100 ) ) )
+            {
+                _result.Clear();
+
+                if ( _searchObject == null )
+                    return;
+
+                string assetPath = AssetDatabase.GetAssetPath( _searchObject );
+                string assetGuid = AssetDatabase.AssetPathToGUID( assetPath );
+                //只检查prefab
+                string[] guids = AssetDatabase.FindAssets( "t:Prefab", new[] { "Assets" } );
+
+                int length = guids.Length;
+                for ( int i = 0; i < length; i++ )
+                {
+                    string filePath = AssetDatabase.GUIDToAssetPath( guids[ i ] );
+                    EditorUtility.DisplayCancelableProgressBar( "Checking", filePath, i / length * 1.0f );
+
+                    //检查是否包含guid
+                    string content = File.ReadAllText( filePath );
+                    if ( content.Contains( assetGuid ) )
+                    {
+                        Object fileObject = AssetDatabase.LoadAssetAtPath( filePath, typeof( Object ) );
+                        _result.Add( fileObject );
+                    }
+                }
+                EditorUtility.ClearProgressBar();
+            }
+            EditorGUILayout.EndHorizontal();
+
+            //显示结果
+            EditorGUILayout.BeginVertical();
+            for ( int i = 0; i < _result.Count; i++ )
+            {
+                EditorGUILayout.ObjectField( _result[ i ], typeof( Object ), true, GUILayout.Width( 300 ) );
+            }
+            EditorGUILayout.EndHorizontal();
+        }
+
+    }
 }

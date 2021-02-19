497ba58c32ad481ab44ed18a40872a2e3e9f159c
pasdedeux <lc10278@163.com>
2021-02-19 19:19:41 +0800

    测试配置

--- Assets/Editor/EditorUse.cs
+++ Assets/Editor/EditorUse.cs
@@ -5,10 +5,33 @@ using UnityEngine;
 
 public class EditorUse
 {
-    [MenuItem( "Tools/UI文件创建" )]
-    public static void UICreate()
+    // Creates a new menu item 'Examples > Create Prefab' in the main menu.
+    [MenuItem( "Tools/创建UI界面" )]
+    static void CreatePrefab()
     {
-        
+        // Keep track of the currently selected GameObject(s)
+        GameObject[] objectArray = Selection.gameObjects;
+
+        // Loop through every GameObject in the array above
+        foreach ( GameObject gameObject in objectArray )
+        {
+            // Set the path as within the Assets folder,
+            // and name it as the GameObject's name with the .Prefab format
+            string localPath = "Assets/" + gameObject.name + ".prefab";
+
+            // Make sure the file name is unique, in case an existing Prefab has the same name.
+            localPath = AssetDatabase.GenerateUniqueAssetPath( localPath );
+
+            // Create the new Prefab.
+            PrefabUtility.SaveAsPrefabAssetAndConnect( gameObject, localPath, InteractionMode.UserAction );
+        }
     }
+
+    //// Disable the menu item if no selection is in place.
+    //[MenuItem( "Examples/Create Prefab", true )]
+    //static bool ValidateCreatePrefab()
+    //{
+    //    return Selection.activeGameObject != null && !EditorUtility.IsPersistent( Selection.activeGameObject );
+    //}
 }
 

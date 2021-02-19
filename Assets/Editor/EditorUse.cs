76452e19d162e6c04557d48bca91bbcd407e4485
pasdedeux <lc10278@163.com>
2021-02-19 17:57:22 +0800

    删除多余编辑器扩展

--- Assets/Editor/EditorUse.cs
+++ Assets/Editor/EditorUse.cs
@@ -5,28 +5,10 @@ using UnityEngine;
 
 public class EditorUse
 {
-
-#if UNITY_EDITOR
-    [MenuItem( "Tools/配置文件1", priority = 20 )]
-#endif
-    public static void XlsxToCSV()
+    [MenuItem( "Tools/UI文件创建" )]
+    public static void UICreate()
     {
-        FolderCopy.CopyTo( Application.dataPath + "/Resources", Application.dataPath + "/Resources1" );
-#if UNITY_EDITOR
-        AssetDatabase.Refresh();
-#endif
+        
     }
-
-#if UNITY_EDITOR
-    [MenuItem( "Tools/配置文件2", priority = 20 )]
-#endif
-    public static void XlsxToCSV2()
-    {
-        FolderCopy.MoveTo( Application.dataPath + "/Resources1", Application.dataPath + "/Resources2" );
-#if UNITY_EDITOR
-        AssetDatabase.Refresh();
-#endif
-    }
-
 }
 

0ae097da62aad7d388a62582b65f93dbd7a65bf7
pasdedeux <lc10278@163.com>
2021-03-22 20:06:33 +0800

    更新层级

--- LitFrameworkEditor/Extention_Editor/MenuExtention/BuildSceneSettingEditor.cs
+++ LitFrameworkEditor/Extention_Editor/MenuExtention/BuildSceneSettingEditor.cs
@@ -31,7 +31,7 @@ public static class BuildSceneSettingEditor
     /// <summary>
     /// 构建场景设置为所有
     /// </summary>
-    [MenuItem( "Tools/Build/同步Scenes场景到SceneSetting" )]
+    [MenuItem( "Tools/Build/同步Scenes场景到SceneSetting", priority = 20 )]
     public static void AddAllScenesToBuildSettings()
     {
         HashSet<string> sceneNames = new HashSet<string>();
@@ -59,7 +59,7 @@ public static class BuildSceneSettingEditor
     /// <summary>
     /// 删除所有的构建场景
     /// </summary>
-    [MenuItem( "Tools/Build/删除所有构建场景" )]
+    [MenuItem( "Tools/Build/删除所有构建场景",priority =21 )]
     public static void DeleteScenesFormBuildSettings()
     {
         List<EditorBuildSettingsScene> scenes = new List<EditorBuildSettingsScene>();

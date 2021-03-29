c451b89e4cebd51da8a0ff407edc3094f6fb1c31
pasdedeux <lc10278@163.com>
2021-03-29 19:36:49 +0800

    增加raycast线框检测器 版本来源于 https://www.itread01.com/content/1547962216.html

--- LitFrameworkEditor/Extention_Editor/MenuExtention/UGUIRaycastCheck.cs
+++ LitFrameworkEditor/Extention_Editor/MenuExtention/UGUIRaycastCheck.cs
@@ -14,15 +14,111 @@
 *******************************************************************
 ======================================*/
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace LitFrameworkEditor.Extention_Editor.MenuExtention
 {
-    public class UGUIRaycastCheck
+    /// <summary>
+    /// RayCast线框显示
+    /// </summary>
+    public class UGUIRaycastCheck: EditorWindow
     {
+        private MaskableGraphic[] graphics;
+        private bool hideUnchecked = false;
+        private bool showBorders = true;
+        private Color borderColor = Color.blue;
+        private Vector2 scrollPosition = Vector2.zero;
+
+        private static UGUIRaycastCheck _selfTarget = null;
+
+        [MenuItem( "Tools/RaycastTarget检测器" )]
+        private static void Open()
+        {
+            _selfTarget = _selfTarget ?? EditorWindow.GetWindow<UGUIRaycastCheck>( "RaycastTarget检测器" );
+            _selfTarget.Show();
+        }
+
+        void OnEnable()
+        {
+            _selfTarget = this;
+        }
+
+        void OnDisable()
+        {
+            _selfTarget = null;
+        }
+
+        void OnGUI()
+        {
+            using ( EditorGUILayout.HorizontalScope horizontalScope = new EditorGUILayout.HorizontalScope() )
+            {
+                showBorders = EditorGUILayout.Toggle( "Show Gizmos", showBorders, GUILayout.Width( 200.0f ) );
+                borderColor = EditorGUILayout.ColorField( borderColor );
+            }
+            hideUnchecked = EditorGUILayout.Toggle( "隐藏未勾选对象", hideUnchecked );
+
+            GUILayout.Space( 12.0f );
+            Rect rect = GUILayoutUtility.GetLastRect();
+            GUI.color = new Color( 0.0f, 0.0f, 0.0f, 0.25f );
+            GUI.DrawTexture( new Rect( 0.0f, rect.yMin + 6.0f, Screen.width, 4.0f ), EditorGUIUtility.whiteTexture );
+            GUI.DrawTexture( new Rect( 0.0f, rect.yMin + 6.0f, Screen.width, 1.0f ), EditorGUIUtility.whiteTexture );
+            GUI.DrawTexture( new Rect( 0.0f, rect.yMin + 9.0f, Screen.width, 1.0f ), EditorGUIUtility.whiteTexture );
+            GUI.color = Color.white;
+
+            graphics = GameObject.FindObjectsOfType<MaskableGraphic>();
+
+            using ( GUILayout.ScrollViewScope scrollViewScope = new GUILayout.ScrollViewScope( scrollPosition ) )
+            {
+                scrollPosition = scrollViewScope.scrollPosition;
+                for ( int i = 0; i < graphics.Length; i++ )
+                {
+                    MaskableGraphic graphic = graphics[ i ];
+                    if ( hideUnchecked == false || graphic.raycastTarget == true )
+                    {
+                        DrawElement( graphic );
+                    }
+                }
+            }
+            foreach ( var item in graphics )
+            {
+                EditorUtility.SetDirty( item );
+            }
+            Repaint();
+        }
+
+        private void DrawElement( MaskableGraphic graphic )
+        {
+            using ( EditorGUILayout.HorizontalScope horizontalScope = new EditorGUILayout.HorizontalScope() )
+            {
+                Undo.RecordObject( graphic, "Modify RaycastTarget" );
+                graphic.raycastTarget = EditorGUILayout.Toggle( graphic.raycastTarget, GUILayout.Width( 20 ) );
+                EditorGUI.BeginDisabledGroup( true );
+                EditorGUILayout.ObjectField( graphic, typeof( MaskableGraphic ), true );
+                EditorGUI.EndDisabledGroup();
+            }
+        }
+
+        [DrawGizmo( GizmoType.Selected | GizmoType.NonSelected )]
+        private static void DrawGizmos( MaskableGraphic source, GizmoType gizmoType )
+        {
+            if ( _selfTarget != null && _selfTarget.showBorders == true && source.raycastTarget == true )
+            {
+                Vector3[] corners = new Vector3[ 4 ];
+                source.rectTransform.GetWorldCorners( corners );
+                Gizmos.color = _selfTarget.borderColor;
+                for ( int i = 0; i < 4; i++ )
+                {
+                    Gizmos.DrawLine( corners[ i ], corners[ ( i + 1 ) % 4 ] );
+                }
+                if ( Selection.activeGameObject == source.gameObject )
+                {
+                    Gizmos.DrawLine( corners[ 0 ], corners[ 2 ] );
+                    Gizmos.DrawLine( corners[ 1 ], corners[ 3 ] );
+                }
+            }
+            SceneView.RepaintAll();
+        }
     }
 }

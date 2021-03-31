ab2b05cc04149e068e072a09afbab4ac1824ce1a
DerekLiu <lc10278@163.com>
2021-03-31 11:58:45 +0800

    修改DEMO

---
+++ Assets/Scripts/View/LoadingStartDemo.cs
@@ -0,0 +1,27 @@
+﻿using LitFramework;
+using LitFramework.GameFlow;
+using LitFramework.Input;
+using LitFramework.LitTool;
+using LitFramework.Mono;
+using UnityEngine;
+
+public class LoadingStartDemo : MonoBehaviour
+{
+    private AsyncOperation _asyncOperation;
+
+    void Start()
+    {
+        LitFrameworkFacade.Instance.StartUp( () =>
+        {
+            //启用UIMANAGER 渐变功能
+            UIManager.Instance.UseFading = true;
+            UIManager.Instance.FadeImage.CrossFadeAlpha( 0, 0.4f, false );
+            ColorUtility.TryParseHtmlString( "#0B477B", out Color color );
+            UIMaskManager.Instance.SetMaskColor( color );
+        } );
+
+        //GameFlowController.Instance.ChangeScene( 1, loadingUIPath: ResPath.UI.UILOADING, needFading: true );
+    }
+
+    
+}

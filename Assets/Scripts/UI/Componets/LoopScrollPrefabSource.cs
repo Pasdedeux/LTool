d33b1ce3254b4ca031d175d51188d234af505455
DerekLiu <lc10278@163.com>
2021-10-29 15:31:14 +0800

    UI滚动列表

---
+++ Assets/Scripts/UI/Componets/LoopScrollPrefabSource.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using LitFramework.LitPool;
+
+namespace UnityEngine.UI
+{
+    [System.Serializable]
+    public class LoopScrollPrefabSource 
+    {
+        public string prefabName;
+        public int poolSize = 5;
+
+        private bool inited = false;
+        public virtual GameObject GetObject()
+        {
+            var go = SpawnManager.Instance.SpwanObject(prefabName);
+            go.transform.localScale = Vector3.one;
+            return go;
+        }
+
+        public virtual void ReturnObject( Transform go )
+        {
+            go.SendMessage( "ScrollCellReturn", SendMessageOptions.DontRequireReceiver );
+            SpawnManager.Instance.DespawnObject( go );
+        }
+    }
+}

2b759c8a35cd5d13f44ac819892b6f332c3afef9
DerekLiu <lc10278@163.com>
2022-01-12 11:45:07 +0800

    更新UI调用框架

--- Assets/Scripts/UI/GUITest.cs
+++ Assets/Scripts/UI/GUITest.cs
@@ -25,101 +25,17 @@ public class GUITest : MonoBehaviour
         int index = 0, vindex = 0 ;
         if (GUI.Button(new Rect(10 + 110 * index++, 100 + 110* vindex, 100, 100), "stack1"))
         {
-            UIManager.Instance.Show(ResPath.UI.UISTACK1);
-        }
-        if (GUI.Button(new Rect(10 + 110 * index++, 100 + 110 * vindex, 100, 100), "stack2"))
-        {
-            UIManager.Instance.Show(ResPath.UI.UISTACK2);
-        }
-        if (GUI.Button(new Rect(10 + 110 * index++, 100 + 110 * vindex, 100, 100), "stack3"))
-        {
-            UIManager.Instance.Show(ResPath.UI.UISTACK3);
-        }
-        if (GUI.Button(new Rect(10 + 110 * index++, 100 + 110 * vindex, 100, 100), "close stack1"))
-        {
-            UIManager.Instance.Close(ResPath.UI.UISTACK1);
-        }
-        if (GUI.Button(new Rect(10 + 110 * index++, 100 + 110 * vindex, 100, 100), "close stack2"))
-        {
-            UIManager.Instance.Close(ResPath.UI.UISTACK2);
-        }
-        if (GUI.Button(new Rect(10 + 110 * index++, 100 + 110 * vindex, 100, 100), "close stack3"))
-        {
-            UIManager.Instance.Close(ResPath.UI.UISTACK3);
         }
         //第二行
         index = 0;vindex++;
         if (GUI.Button(new Rect(10 + 110 * index++, 100 + 110 * vindex, 100, 100), "prallel1"))
         {
-            UIManager.Instance.Show(ResPath.UI.UIPARRALLEL1);
-        }
-        if (GUI.Button(new Rect(10 + 110 * index++, 100 + 110 * vindex, 100, 100), "prallel2"))
-        {
-            UIManager.Instance.Show(ResPath.UI.UIPARRALLEL2);
-        }
-        if (GUI.Button(new Rect(10 + 110 * index++, 100 + 110 * vindex, 100, 100), "prallel3"))
-        {
-            UIManager.Instance.Show(ResPath.UI.UIPARRALLEL3);
-        }
-        if (GUI.Button(new Rect(10 + 110 * index++, 100 + 110 * vindex, 100, 100), "close prallel1"))
-        {
-            UIManager.Instance.Close(ResPath.UI.UIPARRALLEL1);
-        }
-        if (GUI.Button(new Rect(10 + 110 * index++, 100 + 110 * vindex, 100, 100), "close prallel2"))
-        {
-            UIManager.Instance.Close(ResPath.UI.UIPARRALLEL2);
-        }
-        if (GUI.Button(new Rect(10 + 110 * index++, 100 + 110 * vindex, 100, 100), "close prallel3"))
-        {
-            UIManager.Instance.Close(ResPath.UI.UIPARRALLEL3);
         }
         //第三行
         index = 0; vindex++;
         if (GUI.Button(new Rect(10 + 110 * index++, 100 + 110 * vindex, 100, 100), "unique1"))
         {
-            UIManager.Instance.Show(ResPath.UI.UIUNIQUE1);
-        }
-        if (GUI.Button(new Rect(10 + 110 * index++, 100 + 110 * vindex, 100, 100), "unique2"))
-        {
-            UIManager.Instance.Show(ResPath.UI.UIUNIQUE2);
-        }
-        if (GUI.Button(new Rect(10 + 110 * index++, 100 + 110 * vindex, 100, 100), "unique3"))
-        {
-            UIManager.Instance.Show(ResPath.UI.UIUNIQUE3);
-        }
-        if (GUI.Button(new Rect(10 + 110 * index++, 100 + 110 * vindex, 100, 100), "close unique1"))
-        {
-            UIManager.Instance.Close(ResPath.UI.UIUNIQUE1);
-        }
-        if (GUI.Button(new Rect(10 + 110 * index++, 100 + 110 * vindex, 100, 100), "close unique2"))
-        {
-            UIManager.Instance.Close(ResPath.UI.UIUNIQUE2);
-        }
-        if (GUI.Button(new Rect(10 + 110 * index++, 100 + 110 * vindex, 100, 100), "close unique3"))
-        {
-            UIManager.Instance.Close(ResPath.UI.UIUNIQUE3);
-        }
-
-        index = 0; vindex++;
-        if (GUI.Button(new Rect(10 + 110 * index++, 100 + 110 * vindex, 100, 100), "CloseAll(false)"))
-        {
-            UIManager.Instance.CloseAll(isDestroy: true,useAnim:true);
-        }
-        if (GUI.Button(new Rect(10 + 110 * index++, 100 + 110 * vindex, 100, 100), "CloseAll(true)"))
-        {
-            UIManager.Instance.CloseAll(isDestroy: true, useAnim: true, force: true);
         }
     }
 
-}
-
-
-public class TesttClass
-{
-    public int sssvalue = 10;
-    public string ssname = "name";
-    public float sss = 13f;
-
-    public Vector3 pos = new Vector3(10f, 10f, 10f);
-    public Vector2 pos2d = new Vector2(10f, 10f);
 }

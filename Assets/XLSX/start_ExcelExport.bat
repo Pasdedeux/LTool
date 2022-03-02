1ca7f1dc65322a2b6d4fd8eb7403345a491e9a9c
DerekLiu <lc10278@163.com>
2022-03-02 18:19:47 +0800

    修改工具配置

---
+++ Assets/XLSX/start_ExcelExport.bat
@@ -0,0 +1,3 @@
+cd /d ..\..\Tools\App\ExcelTool\bin\Release\net6.0
+dotnet Litframework.ExcelTool.dll --ProjectPath="C:/Personal/Unity/iFunTech/BasePackage/Assets" --ExportModelType=2 --UseHotFix=true --ExtralFileExtention="json|dat|assetbundle"
+pause
--- Assets/XLSX/win_startExcelExport.bat
+++
@@ -1,3 +0,0 @@
-cd /d C:\Personal\Unity\iFunTech\BasePackage\Excel\ExcelTool\bin\Debug\net6.0
-dotnet Litframework.ExcelTool.dll --ProjectPath="C:/Personal/Unity/iFunTech/BasePackage/Assets" --ExportModelType=2 --UseHotFix=true --ExtralFileExtention="json|dat|assetbundle"
-pause

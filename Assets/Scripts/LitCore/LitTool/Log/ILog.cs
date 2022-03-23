04ada63d80453a8daf00a2d38c3d895f5b9e3284
Pasdedeux <lc10278@163.com>
2022-03-23 23:16:52 +0800

    引入LOG组件

---
+++ Assets/Scripts/LitCore/LitTool/Log/ILog.cs
@@ -0,0 +1,17 @@
+﻿public interface ILog
+{
+    bool Enable { get; set; }
+
+    void Trace(string message, LogColor logColor = LogColor.green);
+    void Info(string message, LogColor logColor = LogColor.green);
+    void Warning(string message, LogColor logColor = LogColor.green);
+    void Debug(string message, LogColor logColor = LogColor.green);
+    void Error(string message, LogColor logColor = LogColor.green);
+
+
+    void Trace(string message, params object[] args);
+    void Warning(string message, params object[] args);
+    void Info(string message, params object[] args);
+    void Debug(string message, params object[] args);
+    void Error(string message, params object[] args);
+}
---
+++ Assets/Scripts/LitCore/LitTool/Log/NLogger.cs
@@ -0,0 +1,87 @@
+﻿#if true//NOT_UNITY
+using NLog;
+
+public class NLogger : ILog
+{
+    private readonly Logger logger;
+    public bool Enable { get; set; } = true;
+
+    public NLogger(string name)
+    {
+        Enable = true;
+        this.logger = LogManager.GetLogger(name);
+    }
+
+    public void Trace(string message, LogColor logColor = LogColor.white)
+    {
+        if (!Enable) return;
+        this.logger.Trace(message);
+    }
+
+    public void Warning(string message, LogColor logColor = LogColor.white)
+    {
+        if (!Enable) return;
+        this.logger.Warn(message);
+    }
+
+    public void Info(string message, LogColor logColor = LogColor.white)
+    {
+        if (!Enable) return;
+        this.logger.Info(message);
+    }
+
+    public void Debug(string message, LogColor logColor = LogColor.white)
+    {
+        if (!Enable) return;
+        this.logger.Debug(message);
+    }
+
+    public void Error(string message, LogColor logColor = LogColor.white)
+    {
+        if (!Enable) return;
+        this.logger.Error(message);
+    }
+
+    public void Fatal(string message)
+    {
+        if (!Enable) return;
+        this.logger.Fatal(message);
+    }
+
+    public void Trace(string message, params object[] args)
+    {
+        if (!Enable) return;
+        this.logger.Trace(message, args);
+    }
+
+    public void Warning(string message, params object[] args)
+    {
+        if (!Enable) return;
+        this.logger.Warn(message, args);
+    }
+
+    public void Info(string message, params object[] args)
+    {
+        if (!Enable) return;
+        this.logger.Info(message, args);
+    }
+
+    public void Debug(string message, params object[] args)
+    {
+        if (!Enable) return;
+        this.logger.Debug(message, args);
+    }
+
+    public void Error(string message, params object[] args)
+    {
+        if (!Enable) return;
+        this.logger.Error(message, args);
+    }
+
+    public void Fatal(string message, params object[] args)
+    {
+        if (!Enable) return;
+        this.logger.Fatal(message, args);
+    }
+}
+#endif

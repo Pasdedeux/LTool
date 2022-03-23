30b5afd3b8650fb2febced8eb5828ce9734da98a
Pasdedeux <lc10278@163.com>
2022-03-24 00:04:07 +0800

    更新内容

--- Assets/Scripts/RuntimeScript/HotFixLogic/CSV/SpawnConfig.cs
+++ Assets/Scripts/RuntimeScript/HotFixLogic/CSV/SpawnConfig.cs
@@ -49,7 +49,7 @@ public partial class SpawnConfig
 			}
 			catch (Exception e)
 			{
-				LDebug.LogError($"{e.Message} 表: SpawnConfig 行: {i}列: ID", LogColor.red); 
+				Log.Error($"{e.Message} 表: SpawnConfig 行: {i}列: ID", LogColor.red); 
 			}
 		}
 		return vec;

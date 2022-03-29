cefc88808455fbf92740774679c9b7a80b7cecff
Pasdedeux <lc10278@163.com>
2022-03-29 11:43:47 +0800

    register

--- Client/Assets/Scripts/UI/GUITest.cs
+++ Client/Assets/Scripts/UI/GUITest.cs
@@ -23,55 +23,22 @@ public class GUITest : MonoBehaviour
     {
         //第一行
         int index = 0, vindex = 0 ;
-        if (GUI.Button(new Rect(10 + 110 * index++, 100 + 110 * vindex, 100, 100), "Resource"))
+        if (GUI.Button(new Rect(10 + 110 * index++, 100 + 110 * vindex, 100, 100), "Func1"))
         {
-            _resourceItem = RsLoadManager.Instance.Get("Prefabs/Test/Image_1").transform;
-        }
-        if (GUI.Button(new Rect(10 + 110 * index++, 100 + 110 * vindex, 100, 100), "Spawn"))
-        {
-            _spwanItem = RsLoadManager.Instance.Get("Image_2");
-        }
-        if (GUI.Button(new Rect(10 + 110 * index++, 100 + 110 * vindex, 100, 100), "Spawn"))
-        {
-            SpawnManager.Instance.CreatePool("Prefabs/Test/Image_1", "UI");
+            VibrateManager.Instance.Shake(VibrateState.Interval);
         }
 
         index = 0; vindex++;
-        if (GUI.Button(new Rect(10 + 110 * index++, 100 + 110 * vindex, 100, 100), "Res Destroy"))
+        if (GUI.Button(new Rect(10 + 110 * index++, 100 + 110 * vindex, 100, 100), "Func2"))
         {
-            _resourceItem.gameObject.Destroy();
+            VibrateManager.Instance.Shake(VibrateState.Acute);
         }
-        if (GUI.Button(new Rect(10 + 110 * index++, 100 + 110 * vindex, 100, 100), "Spawn Destroy"))
+
+        index = 0; vindex++;
+        if (GUI.Button(new Rect(10 + 110 * index++, 100 + 110 * vindex, 100, 100), "Func3"))
         {
-            _spwanItem.Destroy();
-        }
-        if (GUI.Button(new Rect(10 + 110 * index++, 100 + 110 * vindex, 100, 100), "RemoveByTag"))
-        {
-            SpawnManager.Instance.RemovePool("UI");
-        }
-        if (GUI.Button(new Rect(10 + 110 * index++, 100 + 110 * vindex, 100, 100), "RemoveByTransform"))
-        {
-            SpawnManager.Instance.RemovePool(_resourceItem);
-        }
-        if (GUI.Button(new Rect(10 + 110 * index++, 100 + 110 * vindex, 100, 100), "RemoveByGameObject"))
-        {
-            SpawnManager.Instance.RemovePool(_spwanItem);
+            VibrateManager.Instance.Shake(VibrateState.Softly);
         }
     }
 
-}
-
-public class TestInfo
-{
-    public Color ss = new Color(200f / 255, 200 / 255f, 155 / 255f);
-    public Vector3 v3 = new Vector3(20, 15, 19f);
-    public Quaternion qq = new Quaternion(1, 2.2f, 4.55666f, 7.8888f);
-    public Cust Test = new Cust { ss = 12, sss = new Vector3(25f, 12f, 1), ssss = Color.red };
-}
-
-public struct Cust
-{
-    public int ss;
-    public Vector3 sss;
-    public Color ssss;
 }
